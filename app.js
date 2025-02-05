b3c43abad3cb95667e55c2bc324d09448d55d930
GiuntaGiovanni <jhonniest741@gmail.com>
2025-02-05 14:50:06 +0100

    [feat] import & initialization of express

---
+++ app.js
@@ -0,0 +1,7 @@
+// importiamo express
+const express = require ('express');
+// inizializziamo express tramite funzione e salviamolo sull'app
+const app = express();
+// salviamo la porta del server
+const port = 3000;
+
