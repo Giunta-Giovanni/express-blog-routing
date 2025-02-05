9a8c98d51c3f492301fb314c6992b1bae2c666d2
GiuntaGiovanni <jhonniest741@gmail.com>
2025-02-05 16:13:30 +0100

    [feat] return array object in route index & return object in route show

--- routers/posts.js
+++ routers/posts.js
@@ -2,14 +2,15 @@
 const express = require ('express');
 // creiamo una variabile router il cui valore sarà un istanza di express.Router()
 const router = express.Router();
-
+// importiamo l'array con i post e lo salviamo in una variabile
+const myPosts = require ('../data/posts')
 
 // definiamo le rotte rimuovendo il nome dalla risposta uri
 router.get('/', function (req,res){
-    res.send ('Visualizzare tutti i post')
+    res.json(myPosts);
 });
 router.get('/:id', function (req,res){
-    res.send (`Visualizza il post numero ${req.params.id}`)
+    res.json (myPosts[req.params.id])
 });
 router.post('/', function (req,res){
     res.send ('crea un nuovo post')
